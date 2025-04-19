52021ef22e2c906e4ff0df0b639a82b748de5dd9
Matheus Felipe <theusf40@hotmail.com>
2025-04-19 12:27:29 -0300

    Changed transformTaskintoText function to getTasksFromNote

--- plugin.js
+++ plugin.js
@@ -80,9 +80,8 @@ const plugin = {
      * @param {string} text With the tasks separated by linebreaks
      * @returns {void}
      */
-    async _batchTagTasks(app, text) {
+    async _batchTagTasks(app, tasks) {
         const systemNotes = await app.filterNotes();
-        const tasksToTag = await this._transformTextIntoTaskArray(app, text);
 
         const inlineTag = await app.prompt("Choose an inline tag", {
             inputs: [
@@ -96,7 +95,7 @@ const plugin = {
         console.log(inlineTag)
 
         // Add the tag to the task name
-        await Promise.all(tasksToTag.map(async task => {
+        await Promise.all(tasks.map(async task => {
             const newtaskName = ` ${task.content}\n[${inlineTag.name}](https://www.amplenote.com/notes/${inlineTag.uuid})`;
             await app.updateTask(task.uuid, { content: newtaskName });
         }));
@@ -290,53 +289,26 @@ const plugin = {
      * @param {string} noteUUID 
      * @returns {string} with the tasks, each separated by a line break
      */
-    async _transformTaskIntoText(app, noteUUID) {
+    async _getTasksFromNote(app, noteUUID) {
         const noteTasks = await app.getNoteTasks({ uuid: noteUUID });
         if (!noteTasks) throw new Error("Current note has no tasks");
-
+        
         // Transform the noteTasks into an object array to match the inputs prompt
         const taskOptions = noteTasks.map(task => ({
             label: task.content,
             type: 'checkbox'
         }));
-
-        const selectedTasks = await app.prompt('Select the tasks you want to act on', {
+        
+        const inputTasks = await app.prompt('Select the tasks you want to act on', {
             inputs: taskOptions
         });
+        
+        if (!inputTasks) throw new Error("Choose at least one tasks in order to proceed");
+        
+        //Filters the tasks that were selected
+        const selectedTasks = noteTasks.filter((_, index) => inputTasks[index]);
 
-        if (!selectedTasks) throw new Error("Choose at least one tasks in order to proceed");
-
-        // Add all of the task names into a single variable to send to the function
-        let taskNames = '';
-
-        for (let i = 0; i < selectedTasks.length; i++) {
-            if (!selectedTasks[i])
-                continue;
-
-            let count = i;
-
-            let rightTask = taskOptions.find(el => {
-                if (count == 0) return true;
-                count--;
-            });
-
-            if (rightTask && rightTask.label) {
-                let contentToAdd = rightTask.label;
-
-                // Removes new lines
-                contentToAdd = contentToAdd.replace(/\\[\r\n]+/g, ' ');
-
-                // Removes the links if there are any
-                contentToAdd = contentToAdd.replace(/(?:__|[*#])|\[(.*?)\]\(.*?\)/gm, '$1');
-
-                taskNames += `${contentToAdd}\n`;
-                console.log(contentToAdd);
-            }
-        }
-
-        taskNames = taskNames.trim();
-
-        return taskNames;
+        return selectedTasks;
     },
 
     /**
